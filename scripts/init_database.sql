eb1cd37a19d28896490a107ab97c8897c3738a51
Abdallah Hamdy <abdallahhamdy101@gmail.com>
2025-08-07 03:49:36 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,36 @@
+USE master ;
+GO
+
+--	drop DataWarehouse database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name= 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE ;
+	DROP DATABASE DataWarehouse ;
+END
+GO
+
+--	create DataWarehouse Database
+CREATE DATABASE DataWarehouse ;
+GO
+
+USE  DataWarehouse ;
+GO
+
+--	create schemas
+CREATE SCHEMA bronze ;
+GO
+
+CREATE SCHEMA silver ;
+GO
+
+CREATE SCHEMA gold ;
+GO
+
+
+
+
+
+
+
+
 
